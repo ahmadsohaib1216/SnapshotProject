e3480bd6cc9d2a4e0058558badaf3d6b06c8b310
Sohaib Ahmad <ahmadsohaib1216@gmail.com>
2019-02-26 17:22:26 +0000

    New Snapshot.py

--- snapshot.py
+++ snapshot.py
@@ -1,8 +1,14 @@
 import boto3
+import click
 
 session = boto3.Session(profile_name='sohaib')
-
 ec2 = session.resource('ec2')
 
-for i in ec2.instances.all():
-	print(i)
+@click.command()
+def list_all_instances():
+	"List Ec2 Instances"
+	for i in ec2.instances.all():
+		print(i)
+
+if __name__ == '__main__':
+list_all_instances()
